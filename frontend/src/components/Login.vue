6f13dbde1d619226e2add90a2baac72cd08ed731
inchingforward <inchingforward@gmail.com>
2016-08-31 22:13:21 -0500

    Started on login component.

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="Login">
+        <h1>Login</h1>
+        <label for="username">Username</label> 
+        <input type="text" id="username" />
+
+        <label for="password">Password</label>
+        <input type="password" id="password" />
+    </div>
+</template>
