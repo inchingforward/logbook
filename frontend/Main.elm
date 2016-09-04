c15a965b093abaa7ef97aa1a2ba99f009aef1292
inchingforward <inchingforward@gmail.com>
2016-09-04 15:15:38 -0500

    Added login form fields.

--- frontend/Main.elm
+++ frontend/Main.elm
@@ -1,7 +1,27 @@
 module Main exposing (..)
 
-import Html
+import Html exposing (Html, button, div, input, text)
+import Html.App as App
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
 main =
-    Html.text "Hello, world!"
+    App.beginnerProgram { model = 0, view = view, update = update }
+
+
+type Msg
+    = Login
+
+
+update msg model =
+    model
+
+
+view model =
+    div []
+        [ text "Login"
+        , input [ type' "text", placeholder "Username" ] []
+        , input [ type' "password", placeholder "Password" ] []
+        , button [ onClick Login ] [ text "Login" ]
+        ]
