9208a9d7d9322b387ae941861352d9a8de490846
inchingforward <inchingforward@gmail.com>
2016-09-05 20:06:37 -0500

    Updating model with username and password fields.

--- frontend/Main.elm
+++ frontend/Main.elm
@@ -3,25 +3,64 @@ module Main exposing (..)
 import Html exposing (Html, button, div, input, text)
 import Html.App as App
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
 
 
+main : Program Never
 main =
-    App.beginnerProgram { model = 0, view = view, update = update }
+    App.beginnerProgram { model = model, view = view, update = update }
+
+
+
+-- Model
+
+
+type alias Model =
+    { username : String
+    , password : String
+    }
+
+
+model : Model
+model =
+    { username = ""
+    , password = ""
+    }
 
 
 type Msg
-    = Login
+    = ChangeUsername String
+    | ChangePassword String
+    | Login
 
 
+
+-- Update
+
+
+update : Msg -> Model -> Model
 update msg model =
-    model
+    case msg of
+        ChangeUsername username ->
+            { model | username = username }
+
+        ChangePassword password ->
+            { model | password = password }
+
+        Login ->
+            model
 
 
+
+-- View
+
+
+view : Model -> Html Msg
 view model =
     div []
         [ text "Login"
-        , input [ type' "text", placeholder "Username" ] []
-        , input [ type' "password", placeholder "Password" ] []
+        , input [ type' "text", placeholder "Username", onInput ChangeUsername ] []
+        , input [ type' "password", placeholder "Password", onInput ChangePassword ] []
+        , div [] [ text ("User: " ++ model.username ++ " Password: " ++ model.password) ]
         , button [ onClick Login ] [ text "Login" ]
         ]
